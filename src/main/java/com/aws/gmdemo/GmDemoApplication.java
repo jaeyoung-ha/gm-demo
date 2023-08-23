32ac0560da3a9067259c75072555bad0dacdb3db
jaeyoung-ha <yangaa47@gmail.com>
2023-08-23 09:49:56 +0900

    test #5

--- src/main/java/com/aws/gmdemo/GmDemoApplication.java
+++ src/main/java/com/aws/gmdemo/GmDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GmDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GmDemoApplication.class, args);  
+        SpringApplication.run(GmDemoApplication.class, args);    
     }
 
 }
