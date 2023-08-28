cf092e942d9b6e5a73ae0021f9ca10356e181258
jaeyoung-ha <yangaa47@gmail.com>
2023-08-28 16:42:17 +0900

    test logback

---
+++ src/main/java/com/aws/gmdemo/log/LogConfig.java
@@ -0,0 +1,30 @@
+package com.aws.gmdemo.log;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+
+@Configuration
+public class LogConfig {
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @Bean
+    public WebServerFactoryCustomizer embeddedServletContainerCustomizer() {
+        return container -> {
+            if (container instanceof TomcatServletWebServerFactory) {
+                ((TomcatServletWebServerFactory) container).addContextCustomizers((TomcatContextCustomizer) context -> {
+                    LogbackValve valve = new LogbackValve();
+                    valve.setFilename(resourceLoader.getResource(ResourceLoader.CLASSPATH_URL_PREFIX + "logback-access.xml").getFilename());
+                    context.getPipeline().addValve(valve);
+                });
+            }
+        };
+    }
+}
