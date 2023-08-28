5dd1700ea377a7942f3e144af8832e465052856b
jaeyoung-ha <yangaa47@gmail.com>
2023-08-28 18:24:32 +0900

    chagned#3

--- src/main/java/com/aws/gmdemo/log/LogConfig.java
+++ src/main/java/com/aws/gmdemo/log/LogConfig.java
@@ -1,29 +1,21 @@
 package com.aws.gmdemo.log;
 
 import ch.qos.logback.access.tomcat.LogbackValve;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ResourceLoader;
 
 @Configuration
 public class LogConfig {
-
-    @Autowired
-    ResourceLoader resourceLoader;
 
     @Bean
     public WebServerFactoryCustomizer embeddedServletContainerCustomizer() {
         return container -> {
             if (container instanceof TomcatServletWebServerFactory) {
                 ((TomcatServletWebServerFactory) container).addContextCustomizers((TomcatContextCustomizer) context -> {
-                    LogbackValve valve = new LogbackValve();
-//                    valve.setFilename(resourceLoader.getResource(ResourceLoader.CLASSPATH_URL_PREFIX + "logback-access.xml").getFilename());
-                    valve.setFilename("src/main/resources/logback-access.xml");
-                    context.getPipeline().addValve(valve);
+                    context.getPipeline().addValve( new LogbackValve());
                 });
             }
         };
